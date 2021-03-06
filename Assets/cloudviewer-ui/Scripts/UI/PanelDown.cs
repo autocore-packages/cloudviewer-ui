9827339c825274b04c4d37300680adcd939279d6
BaoHan <1004578954@qq.com>
2021-03-06 16:18:34 +0800

    init0.0.1

---
+++ Assets/cloudviewer-ui/Scripts/UI/PanelDown.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts;
+
+namespace Assets.Scripts.UI
+{
+    public class PanelDown : PanelBase<PanelDown>, ISimuPanel
+    {
+        public InputField inputField_get;
+        public InputField inputField_post;
+        public Button button_get;
+        public Button button_set;
+        public Text text_Result;
+        // Start is called before the first frame update
+        void Start()
+        {
+            CVManager.Instance.addressNode.OnAddressConfigChange += SetPanelDown;
+            CVManager.Instance.webRequesetServer.OnPostRequest += SetResultText;
+            inputField_get.onValueChanged.AddListener((string value) =>
+            {
+                CVManager.Instance.webRequesetServer.getAddress = value;
+            }); 
+            inputField_get.onValueChanged.AddListener((string value) =>
+            {
+                CVManager.Instance.webRequesetServer.postAddress = value;
+            });
+            button_get.onClick.AddListener(()=> 
+            {
+                StartCoroutine(CVManager.Instance.webRequesetServer.GetWebRequest());
+            });
+            button_set.onClick.AddListener(() =>
+            {
+                StartCoroutine(CVManager.Instance.webRequesetServer.PostWebRequest("\"light_id\":\"light_3\",\"color\":3,\"remain\":30"));
+            });
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        private void SetPanelDown(AddressConfig config)
+        {
+            inputField_get.text = config.getTrafficAddress.ToString();
+            inputField_post.text = config.postTrafficAddress.ToString();
+        }
+        private void SetResultText(string content)
+        {
+            text_Result.text = content;
+        }
+    }
+}
