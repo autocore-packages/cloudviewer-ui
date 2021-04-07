854c710ac9c76d36eed537383704676a0c3490cd
BaoHan <1004578954@qq.com>
2021-04-07 18:25:51 +0800

    0.0.9

--- Assets/cloudviewer-ui/Scripts/UI/PanelAddressSetting.cs
+++ Assets/cloudviewer-ui/Scripts/UI/PanelAddressSetting.cs
@@ -7,14 +7,10 @@ using Assets.Scripts;
 
 namespace Assets.Scripts.UI
 {
-    public class PanelDown : PanelBase<PanelDown>, ISimuPanel
+    public class PanelAddressSetting : PanelBase<PanelAddressSetting>, ISimuPanel
     {
         public InputField inputField_get;
         public InputField inputField_post;
-        public Button button_get;
-        public Button button_post;
-        public Button button_postForm;
-        public Text text_Result;
         // Start is called before the first frame update
         void Start()
         {
@@ -28,19 +24,6 @@ namespace Assets.Scripts.UI
             inputField_post.onValueChanged.AddListener((string value) =>
             {
                 CVManager.Instance.webRequesetServer.postAddress = value;
-            });
-            button_get?.onClick.AddListener(() =>
-            {
-                Debug.Log("click get");
-                StartCoroutine(CVManager.Instance.webRequesetServer.GetWebRequest());
-            });
-            button_post.onClick.AddListener(() =>
-            {
-                StartCoroutine(CVManager.Instance.webRequesetServer.PostWebRequest("{\"light_id\":\"light_3\",\"color\":3,\"remain\":30}"));
-            });
-            button_postForm.onClick.AddListener(() =>
-            {
-                StartCoroutine(CVManager.Instance.webRequesetServer.PostWebRequest_Form(new TrafficLightData("traffic",1,30)));
             });
         }
 
@@ -57,7 +40,7 @@ namespace Assets.Scripts.UI
         private void SetResultText(string content)
         {
             Debug.Log(content);
-            text_Result.text = content;
+            PanelNotice.Instance.SetNotice(content);
         }
     }
 }
